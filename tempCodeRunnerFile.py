61ec59b02ffd8941eefe0cb32c37b93c14cb6943
xuziao <xuziaoder@gmail.com>
2023-02-28 20:07:12 +0800

    alpha

--- tempCodeRunnerFile.py
+++
@@ -1,7 +0,0 @@
-    appoint_time = input("请输入时间")
-    get_date = appoint_time.split('-')
-    begin_time =  get_date[0:4]
-    begin_time[0] += '年'
-    begin_time[1] += '月'
-    begin_time[2] += '日'
-    print(begin_time)
