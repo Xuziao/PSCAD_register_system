e854a4ff954aebac93b5efc99f322a8982cabc39
xuziao <xuziaoder@gmail.com>
2023-02-28 19:52:05 +0800

    alpha.test

--- main.py
+++ main.py
@@ -19,22 +19,21 @@ def initialize():
         print(error_message)
         with open(file_path,'w') as f:
             write = csv.writer(f)
-            head = ["姓名","起始时间","终止时间"]
+            head = ["姓名","年","月","日","起始时间","终止时间"]
             write.writerow(head) 
         print("已重新生成文件")
     print("数据文件已检查完毕")
 
 def input_appointment():
     name = input("请输入您的姓名：")
-    date.append(name.split())
+    date.append(name)
     print("接下来我将询问您预定使用的时间和持续时间\n请您以阿拉伯数字分别输入并以回车结尾\n例如2023-2-28-13:30-14:30 代表预约在2023.2.28 13：30结束于14：30")
-    appoint_time = input("请输入时间")
+    appoint_time = input("请输入时间:")
     get_date = appoint_time.split('-')
-    begin_time =  get_date[0:4]
-    begin_time[0] += '年'
-    begin_time[1] += '月'
-    begin_time[2] += '日'
-    print(begin_time)
+    get_date[0] += '年'
+    get_date[1] += '月'
+    get_date[2] += '日'
+    date.extend(get_date)
 
 def check_appointment():
     print("正在为您打开预约数据文件，请在浏览完毕后关闭文件")
@@ -44,7 +43,7 @@ def check_appointment():
 def write_date():
     with open(file_path,'a') as f:
         write = csv.writer(f)
-        write.writerows(date)
+        write.writerow(date)
     print("写入成功！")
     os.system('pause')
 
